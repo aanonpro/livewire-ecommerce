c1e183457e0e0dbe37b1a9abecff1847ea7aefb2
aanonpro <anonphin.pro@gmail.com>
2022-07-23 18:53:16 +0700

    edit

---
+++ livewire-ecommerce/resources/views/admin/category/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">CATEGORY
+                    <a href="" class="text-white btn btn-primary btn-sm float-end">Add Category</a></h4>
+                {{-- <p class="card-description">
+                  Add class <code>.table-striped</code>
+                </p> --}}
+                <div class="table-responsive">
+                  <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>User</th>
+                        <th>First name</th>
+                        <th>Progress</th>
+                        <th>Amount</th>
+                        <th>Deadline</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td class="py-1">
+                          <img src="{{ asset('admin/images/faces/face1.jpg')}}" alt="image"/>
+                        </td>
+                        <td>
+                          Herman Beck
+                        </td>
+                        <td>
+                          <div class="progress">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </td>
+                        <td>
+                          $ 77.99
+                        </td>
+                        <td>
+                          May 15, 2015
+                        </td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+    </div>
+</div>
+
+@endsection
---
+++ livewire-ecommerce/resources/views/layouts/inc/admin/sidebar.blade.php
@@ -0,0 +1,74 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-home menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-home menu-icon"></i>
+          <span class="menu-title">Sales</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="mdi mdi-view-list menu-icon"></i>
+          <span class="menu-title">Category</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="">View Category</a></li>
+            <li class="nav-item"> <a class="nav-link" href="">Add Category</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="mdi mdi-plus-circle menu-icon"></i>
+          <span class="menu-title">Products</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="">View Products</a></li>
+            <li class="nav-item"> <a class="nav-link" href="">Add Products</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-home menu-icon"></i>
+          <span class="menu-title">Brands</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="mdi mdi-circle-outline menu-icon"></i>
+          <span class="menu-title">User</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="">View users</a></li>
+            <li class="nav-item"> <a class="nav-link" href="">Add Admin</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-home menu-icon"></i>
+          <span class="menu-title">Home Slider</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-settings menu-icon"></i>
+          <span class="menu-title">Site Setting</span>
+        </a>
+      </li>
+
+    </ul>
+  </nav>
